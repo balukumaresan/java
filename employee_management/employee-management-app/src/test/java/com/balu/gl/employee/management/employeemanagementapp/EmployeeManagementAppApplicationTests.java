43dd181127d70c52e49253ea2cc646115d025413
balu <balukumaresan@gmail.com>
2021-12-30 23:40:30 -0500

    First commit

---
+++ employee_management/employee-management-app/src/test/java/com/balu/gl/employee/management/employeemanagementapp/EmployeeManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.balu.gl.employee.management.employeemanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
