6ae74dfae00a807dc15c5cba352a6402f2d7bbdf
balukumaresan <balukumaresan@gmail.com>
2022-02-26 21:47:02 -0500

    updated

--- todolist.js
+++ todolist.js
@@ -7,14 +7,15 @@ function addTask() {
         var newtaskitem = document.createElement('li');
 
         var taskTxt = document.createTextNode(inputTask);
-        newtaskitem.className = "task";
-        newtaskitem.setAttribute('id',taskTxt.value);
+        newtaskitem.className = "tasklistelement";
+        newtaskitem.setAttribute('id',inputTask);
         newtaskitem.appendChild(taskTxt);
 
         var editButton = document.createElement("button");
         var edittxt = document.createTextNode("Edit");
         editButton.className = "edit";
         editButton.appendChild(edittxt);
+        editButton.addEventListener('click', editItems, false);
         newtaskitem.appendChild(editButton);
 
         var deleteButton = document.createElement("button");
@@ -28,25 +29,73 @@ function addTask() {
 
         document.getElementById('task').value= "";
 
+        var deleteItems = document.getElementsByClassName("delete");
 
+        for (i = 0; i < deleteItems.length; i++) {
+            deleteItems[i].onclick = function() {
+                alert(this.parentElement.innerText);
+              var div = this.parentElement;
+              div.style.display = "none";
+            }
+          }
+            }
+ 
+
+
+}
+
+function editItems(){
+    var inputTask = document.getElementById('task').value;
+    alert(inputTask);
+    if (inputTask == "") {
+        var txt = this.parentElement.innerText;
+        var txt1 = txt.split('Edit')[0]
+        document.getElementById('task').value = txt1;
+
+    }
+    else {
+        var div = this.parentElement;
+              div.style.display = "none";
+        this.parentElement.innerText= (inputTask);
+        var newtaskitem = document.createElement('li');
+
+        var taskTxt = document.createTextNode(inputTask);
+        newtaskitem.className = "tasklistelement";
+        newtaskitem.setAttribute('id',inputTask);
+        newtaskitem.appendChild(taskTxt);
+
+        var editButton = document.createElement("button");
+        var edittxt = document.createTextNode("Edit");
+        editButton.className = "edit";
+        editButton.appendChild(edittxt);
+        editButton.addEventListener('click', editItems, false);
+        newtaskitem.appendChild(editButton);
+
+        var deleteButton = document.createElement("button");
+        var deletetxt = document.createTextNode("Delete");
+        deleteButton.className = "delete";
+        deleteButton.appendChild(deletetxt);
+        newtaskitem.appendChild(deleteButton);
+
+        var taskList = document.getElementById('tasklist');
+        taskList.appendChild(newtaskitem);
+
+        document.getElementById('task').value= "";
+        var deleteItems = document.getElementsByClassName("delete");
+        for (i = 0; i < deleteItems.length; i++) {
+            deleteItems[i].onclick = function() {
+                alert(this.parentElement.innerText);
+              var div = this.parentElement;
+              div.style.display = "none";
+            }
+          }
 
     }
 
-    let deleteBtn = document.getElementsByClassName("delete");
-    Array.prototype.slice.call(deleteBtn).forEach(function (item) {
-        item.addEventListener("click", function (e) {
-            e.target.parentNode.remove()
-        });
 
-    })
 
-    let editBtn = document.getElementsByClassName("edit");
-    Array.prototype.slice.call(editBtn).forEach(function (item) {
-        item.addEventListener("click", function (e) {
-            alert('Hi');
-            alert(this.document.getElementsByClassName('task'));
-        });
 
-    })
+
+
 }
 
